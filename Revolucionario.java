c66aa43a4a20a0d9e81842568416fc107f96a6f4
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2018-07-03 11:00:40 -0500

    Update Revolucionario.java

--- Revolucionario.java
+++ Revolucionario.java
@@ -1,8 +1,5 @@
 package RevolucionR;
-
-import EHCPI.Civil;
-
-public abstract class Revolucionario extends Civil {
+public abstract class Revolucionario{
 	String corriente;
 	public Revolucionario(String nombre, String Ubicacion,String Corriente) {
 		super(nombre, region);
