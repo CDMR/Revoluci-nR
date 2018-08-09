33a8c592eff12b0309b2da7beb0e727c5f3a1e5c
Carlos Daniel Marceleño Rodriguez <carlosd.marceleno@gmail.com>
2018-08-09 16:05:47 -0500

    Update Fariseo.java

--- Fariseo.java
+++ Fariseo.java
@@ -1,4 +1,4 @@
-public abstract class Fariseo extends Presentation implements {
+public abstract class Fariseo extends Presentation implements jud{
 	Fariseo public (String nombre, String region) {
 		super(nombre, region);
 	}
